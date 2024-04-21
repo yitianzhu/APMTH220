db91e84a50056d7f634157873a8ec81d9de433a7
Yitian <zhuyt@mit.edu>
2024-04-21 18:39:56 -0400

    Running with MLP head for downstream task

--- learn_subgraph_mamba.sh
+++ learn_subgraph_mamba.sh
@@ -10,34 +10,34 @@ mkdir -p "$RESULT_FOLDER"
 echo "Running Graph Mamba with hops on dataset: $DATASET"
 echo "Logging results in: $RESULT_FOLDER" 
 
-mkdir -p "$RESULT_FOLDER/1hop_$DATASET"
-python learn_graph_mamba_savegrad.py --dataset "$DATASET" --logfilename "$RESULT_FOLDER/1hop_$DATASET" --n-layers 1 --epochs 100 --seqlength 1000 --hops 1
+#mkdir -p "$RESULT_FOLDER/1hop_$DATASET"
+#python learn_graph_mamba_savegrad.py --dataset "$DATASET" --logfilename "$RESULT_FOLDER/1hop_$DATASET" --n-layers 1 --epochs 100 --seqlength 1000 --hops 1
 mkdir -p "$RESULT_FOLDER/2hop_$DATASET"
 python learn_graph_mamba_savegrad.py --dataset "$DATASET" --logfilename "$RESULT_FOLDER/2hop_$DATASET" --n-layers 1 --epochs 100 --seqlength 3000 --hops 2
-mkdir -p "$RESULT_FOLDER/3hop_$DATASET"
-python learn_graph_mamba_savegrad.py --dataset "$DATASET" --logfilename "$RESULT_FOLDER/3hop_$DATASET" --n-layers 1 --epochs 100 --seqlength 5000 --hops 3
-python plot_train_val.py --dataset-name "$DATASET_NAME" --result-folder "$RESULT_FOLDER" 
-python plot_param_diff_norms.py --dataset-name "$DATASET_NAME" --result-folder "$RESULT_FOLDER" --plot-downstream
-python plot_param_grads.py --dataset-name "$DATASET_NAME" --result-folder "$RESULT_FOLDER" --plot-downstream
+#mkdir -p "$RESULT_FOLDER/3hop_$DATASET"
+#python learn_graph_mamba_savegrad.py --dataset "$DATASET" --logfilename "$RESULT_FOLDER/3hop_$DATASET" --n-layers 1 --epochs 100 --seqlength 5000 --hops 3
+python plot_train_val.py --dataset-name "$DATASET_NAME" --result-folder "$RESULT_FOLDER" --checking-hops
+python plot_param_diff_norms.py --dataset-name "$DATASET_NAME" --result-folder "$RESULT_FOLDER" --plot-downstream --checking-hops
+python plot_param_grads.py --dataset-name "$DATASET_NAME" --result-folder "$RESULT_FOLDER" --plot-downstream --checking-hops
 
 
 
-DATASET="hpo_neruo"
+DATASET="hpo_neuro"
 DATASET_NAME="HPO Neuro"
 RESULT_FOLDER="what_did_we_learn/$DATE_FOLDER/$DATASET"
 mkdir -p "$RESULT_FOLDER"
 echo "Running Graph Mamba with hops on dataset: $DATASET"
 echo "Logging results in: $RESULT_FOLDER" 
 
-mkdir -p "$RESULT_FOLDER/1hop_$DATASET"
-python learn_graph_mamba_savegrad.py --dataset "$DATASET" --logfilename "$RESULT_FOLDER/1hop_$DATASET" --n-layers 1 --epochs 100 --seqlength 1000 --hops 1
-mkdir -p "$RESULT_FOLDER/2hop_$DATASET"
-python learn_graph_mamba_savegrad.py --dataset "$DATASET" --logfilename "$RESULT_FOLDER/2hop_$DATASET" --n-layers 1 --epochs 100 --seqlength 3000 --hops 2
-mkdir -p "$RESULT_FOLDER/3hop_$DATASET"
-python learn_graph_mamba_savegrad.py --dataset "$DATASET" --logfilename "$RESULT_FOLDER/3hop_$DATASET" --n-layers 1 --epochs 100 --seqlength 5000 --hops 3
-python plot_train_val.py --dataset-name "$DATASET_NAME" --result-folder "$RESULT_FOLDER" 
-python plot_param_diff_norms.py --dataset-name "$DATASET_NAME" --result-folder "$RESULT_FOLDER" --plot-downstream
-python plot_param_grads.py --dataset-name "$DATASET_NAME" --result-folder "$RESULT_FOLDER" --plot-downstream
+#mkdir -p "$RESULT_FOLDER/1hop_$DATASET"
+#python learn_graph_mamba_savegrad.py --dataset "$DATASET" --logfilename "$RESULT_FOLDER/1hop_$DATASET" --n-layers 1 --epochs 100 --seqlength 1000 --hops 1
+#mkdir -p "$RESULT_FOLDER/2hop_$DATASET"
+#python learn_graph_mamba_savegrad.py --dataset "$DATASET" --logfilename "$RESULT_FOLDER/2hop_$DATASET" --n-layers 1 --epochs 100 --seqlength 3000 --hops 2
+#mkdir -p "$RESULT_FOLDER/3hop_$DATASET"
+#python learn_graph_mamba_savegrad.py --dataset "$DATASET" --logfilename "$RESULT_FOLDER/3hop_$DATASET" --n-layers 1 --epochs 100 --seqlength 5000 --hops 3
+#python plot_train_val.py --dataset-name "$DATASET_NAME" --result-folder "$RESULT_FOLDER" --checking-hops
+#python plot_param_diff_norms.py --dataset-name "$DATASET_NAME" --result-folder "$RESULT_FOLDER" --plot-downstream --checking-hops
+#python plot_param_grads.py --dataset-name "$DATASET_NAME" --result-folder "$RESULT_FOLDER" --plot-downstream --checking-hops
 
 DATASET="ppi_bp"
 DATASET_NAME="PPI BP"
@@ -46,13 +46,13 @@ mkdir -p "$RESULT_FOLDER"
 echo "Running Graph Mamba with hops on dataset: $DATASET"
 echo "Logging results in: $RESULT_FOLDER" 
 
-mkdir -p "$RESULT_FOLDER/1hop_$DATASET"
-python learn_graph_mamba_savegrad.py --dataset "$DATASET" --logfilename "$RESULT_FOLDER/1hop_$DATASET" --n-layers 1 --epochs 100 --seqlength 1000 --hops 1
-mkdir -p "$RESULT_FOLDER/2hop_$DATASET"
-python learn_graph_mamba_savegrad.py --dataset "$DATASET" --logfilename "$RESULT_FOLDER/2hop_$DATASET" --n-layers 1 --epochs 100 --seqlength 3000 --hops 2
-mkdir -p "$RESULT_FOLDER/3hop_$DATASET"
-python learn_graph_mamba_savegrad.py --dataset "$DATASET" --logfilename "$RESULT_FOLDER/3hop_$DATASET" --n-layers 1 --epochs 100 --seqlength 5000 --hops 3
-python plot_train_val.py --dataset-name "$DATASET_NAME" --result-folder "$RESULT_FOLDER" 
-python plot_param_diff_norms.py --dataset-name "$DATASET_NAME" --result-folder "$RESULT_FOLDER" --plot-downstream
-python plot_param_grads.py --dataset-name "$DATASET_NAME" --result-folder "$RESULT_FOLDER" --plot-downstream
+#mkdir -p "$RESULT_FOLDER/1hop_$DATASET"
+#python learn_graph_mamba_savegrad.py --dataset "$DATASET" --logfilename "$RESULT_FOLDER/1hop_$DATASET" --n-layers 1 --epochs 100 --seqlength 1000 --hops 1
+#mkdir -p "$RESULT_FOLDER/2hop_$DATASET"
+#python learn_graph_mamba_savegrad.py --dataset "$DATASET" --logfilename "$RESULT_FOLDER/2hop_$DATASET" --n-layers 1 --epochs 100 --seqlength 3000 --hops 2
+#mkdir -p "$RESULT_FOLDER/3hop_$DATASET"
+#python learn_graph_mamba_savegrad.py --dataset "$DATASET" --logfilename "$RESULT_FOLDER/3hop_$DATASET" --n-layers 1 --epochs 100 --seqlength 5000 --hops 3
+#python plot_train_val.py --dataset-name "$DATASET_NAME" --result-folder "$RESULT_FOLDER" --checking-hops
+#python plot_param_diff_norms.py --dataset-name "$DATASET_NAME" --result-folder "$RESULT_FOLDER" --plot-downstream --checking-hops
+#python plot_param_grads.py --dataset-name "$DATASET_NAME" --result-folder "$RESULT_FOLDER" --plot-downstream --checking-hops
 
